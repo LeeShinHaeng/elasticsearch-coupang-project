a0c788796f2440b750e1ca2b5c5da92894b6b9b8
LeeShinHeang <dragon11796@gmail.com>
2025-06-19 17:11:56 +0900

    chore: 프로젝트 포맷팅 및 변수 재설정

--- src/main/java/com/example/coupangapiserver/product/ProductController.java
+++ src/main/java/com/example/coupangapiserver/product/ProductController.java
@@ -2,7 +2,6 @@ package com.example.coupangapiserver.product;
 
 import com.example.coupangapiserver.product.domain.Product;
 import com.example.coupangapiserver.product.dto.CreateProductRequestDto;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,32 +12,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("products")
 public class ProductController {
 
-    private final ProductService productService;
+	private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
 
-    @GetMapping()
-    public ResponseEntity<List<Product>> getProducts(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size) {
-        List<Product> products = productService.getProducts(page, size);
-        return ResponseEntity.ok(products);
-    }
+	@GetMapping()
+	public ResponseEntity<List<Product>> getProducts(
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "10") int size
+	) {
+		List<Product> products = productService.getProducts(page, size);
+		return ResponseEntity.ok(products);
+	}
 
 
-    @PostMapping()
-    public ResponseEntity<Product> createProduct(@RequestBody CreateProductRequestDto createProductRequestDto) {
-        Product product = productService.createProduct(createProductRequestDto);
-        return ResponseEntity.ok(product);
-    }
+	@PostMapping()
+	public ResponseEntity<Product> createProduct(@RequestBody CreateProductRequestDto createProductRequestDto) {
+		Product product = productService.createProduct(createProductRequestDto);
+		return ResponseEntity.ok(product);
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
-        return ResponseEntity.noContent().build();
-    }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+		productService.deleteProduct(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/example/coupangapiserver/product/domain/Product.java
+++ src/main/java/com/example/coupangapiserver/product/domain/Product.java
@@ -10,53 +10,53 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "products")
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String name;
+	private String name;
 
-    @Column(columnDefinition = "TEXT")
-    private String description; // 에디터가 HTML로 상품 설명을 저장
+	@Column(columnDefinition = "TEXT")
+	private String description; // 에디터가 HTML로 상품 설명을 저장
 
-    private int price;
+	private int price;
 
-    private double rating;
+	private double rating;
 
-    private String category;
+	private String category;
 
-    public Product() {
-    }
+	public Product() {
+	}
 
-    public Product(String name, String description, int price, double rating, String category) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.rating = rating;
-        this.category = category;
-    }
+	public Product(String name, String description, int price, double rating, String category) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.rating = rating;
+		this.category = category;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public int getPrice() {
-        return price;
-    }
+	public int getPrice() {
+		return price;
+	}
 
-    public double getRating() {
-        return rating;
-    }
+	public double getRating() {
+		return rating;
+	}
 
-    public String getCategory() {
-        return category;
-    }
+	public String getCategory() {
+		return category;
+	}
 }
