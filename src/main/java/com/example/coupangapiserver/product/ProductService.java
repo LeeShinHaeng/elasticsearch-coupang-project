1a04b033381478005b931cd91f10ac8fd05c1f18
LeeShinHeang <dragon11796@gmail.com>
2025-06-21 10:29:19 +0900

    feat: 자동완성 기능 구현

--- src/main/java/com/example/coupangapiserver/product/ProductService.java
+++ src/main/java/com/example/coupangapiserver/product/ProductService.java
@@ -1,25 +1,36 @@
 package com.example.coupangapiserver.product;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.MultiMatchQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.TextQueryType;
 import com.example.coupangapiserver.product.domain.Product;
 import com.example.coupangapiserver.product.domain.ProductDocument;
 import com.example.coupangapiserver.product.domain.ProductDocumentRepository;
 import com.example.coupangapiserver.product.dto.CreateProductRequestDto;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
 
 	private final ProductRepository productRepository;
 	private final ProductDocumentRepository productDocumentRepository;
+	private final ElasticsearchOperations elasticsearchOperations;
 
 	public ProductService(ProductRepository productRepository,
-						  ProductDocumentRepository productDocumentRepository) {
+						  ProductDocumentRepository productDocumentRepository,
+						  ElasticsearchOperations elasticsearchOperations) {
 		this.productRepository = productRepository;
 		this.productDocumentRepository = productDocumentRepository;
+		this.elasticsearchOperations = elasticsearchOperations;
 	}
 
 	public List<Product> getProducts(int page, int size) {
@@ -53,5 +64,28 @@ public class ProductService {
 	public void deleteProduct(Long id) {
 		productRepository.deleteById(id);
 		productDocumentRepository.deleteById(id.toString());
+	}
+
+	public List<String> getSuggestions(String query) {
+		Query multimatchQuery = MultiMatchQuery.of(
+			m -> m.query(query)
+				.type(TextQueryType.BoolPrefix)
+				.fields(
+					"name.auto_complete",
+					"name.auto_complete._2gram",
+					"name.auto_complete._3gram"
+				)
+		)._toQuery();
+
+		NativeQuery nativeQuery = NativeQuery.builder()
+			.withQuery(multimatchQuery)
+			.withPageable(PageRequest.of(0, 5))
+			.build();
+
+		SearchHits<ProductDocument> searchHits = elasticsearchOperations.search(nativeQuery, ProductDocument.class);
+
+		return searchHits.getSearchHits().stream()
+			.map(hit -> hit.getContent().getName())
+			.toList();
 	}
 }
